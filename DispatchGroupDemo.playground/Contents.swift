9ddd175b49e6aff4407bd4924ffc6ab2a5ce01c8
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2022-12-29 14:35:08 +0530

    moved Traveller class to separate file

--- DispatchGroupDemo.playground/Contents.swift
+++ DispatchGroupDemo.playground/Contents.swift
@@ -1,31 +1,5 @@
 import Foundation
 
-
-
-class Traveller {
-    private let name: String
-    private let travelDirection: Direction
-    
-    private let queue = DispatchQueue(
-        label: "com.DispatchGroup.example",
-        attributes: .concurrent
-    )
-    
-    init(name: String, headedIn direction: Direction) {
-        self.name = name
-        self.travelDirection = direction
-    }
-    
-    func startTrip(completion: @escaping () -> Void) {
-        print("\(name) has started trip from \(travelDirection.startingPoint)")
-
-        queue.asyncAfter(deadline: .now() + Double.random(in: 1...5)) { [unowned self] in
-            print("\(name) has reached \(travelDirection.destination) successfully")
-            completion()
-        }
-    }
-}
-
 let gopal = Traveller(name: "Gopal", headedIn: .east)
 let madhav = Traveller(name: "Madhav", headedIn: .west)
 let laxman = Traveller(name: "Laxman", headedIn: .north)
